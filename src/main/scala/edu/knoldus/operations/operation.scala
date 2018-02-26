d9db75c48d7793153db625a0faf945881909fad2
miralgandhi5 <miralgandhi5@gmail.com>
2018-02-26 11:01:12 +0530

    operations added

--- src/main/scala/edu/knoldus/operations/operation.scala
+++ src/main/scala/edu/knoldus/operations/operation.scala
@@ -19,6 +19,20 @@ object operation {
     }
   }
 
+  def getYearlySum: RDD[(String, String)] = {
+    salesDataExtracted.groupBy(yearAndId => (yearAndId._1, yearAndId._4)).map(yearAndIdData =>
+      (yearAndIdData._1._2, yearAndIdData._1._1, "#", "#",
+        yearAndIdData._2.foldLeft(0.toLong)((salesSum, data) => salesSum + data._5))).map(data => (data._1, s"#${data._2}#${data._3}#${data._4}#${data._5}"))
+
+  }
+
+  def getMonthlySum: RDD[(String, String)] = {
+    salesDataExtracted.groupBy(monthAndId => (monthAndId._1, monthAndId._2, monthAndId._4)).map(monthAndIdData =>
+      (monthAndIdData._1._3, monthAndIdData._1._1, monthAndIdData._1._2, "#",
+        monthAndIdData._2.foldLeft(0.toLong)((salesSum, data) => salesSum + data._5))).map(data => (data._1, s"#${data._2}#${data._3}#${data._4}#${data._5}"))
+
+  }
+
   private def salesDataExtracted: RDD[(Int, Int, Int, String, Long)] = {
     rddSales.map {
       data => {
@@ -28,9 +42,12 @@ object operation {
     }
   }
 
-  def getYearlySum: RDD[(String, String)] = {
-    salesDataExtracted.groupBy(yearAndId => (yearAndId._1, yearAndId._4)).map(yearAndIdData =>
-      (yearAndIdData._1._2, yearAndIdData._1._1, "#", "#",
-        yearAndIdData._2.foldLeft(0.toLong)((accumulate, tuple) => accumulate + tuple._5))).map(data => (data._1, s"#${data._2}#${data._3}#${data._4}#${data._5}"))
+  def getAllSum: RDD[(String, String)] = {
+    salesDataExtracted.groupBy(dateAndId => (dateAndId._1, dateAndId._2, dateAndId._3, dateAndId._4)).map(dateAndIdData =>
+      (dateAndIdData._1._4, dateAndIdData._1._1, dateAndIdData._1._2, dateAndIdData._1._3,
+        dateAndIdData._2.foldLeft(0.toLong)((salesSum, data) => salesSum + data._5))).map(data => (data._1, s"#${data._2}#${data._3}#${data._4}#${data._5}"))
 
+  }
+
+ 
 }
